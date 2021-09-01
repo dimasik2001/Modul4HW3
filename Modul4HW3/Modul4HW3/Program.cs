b1012be8ef3ab927038f953d7b6cf8c02d2e0bc8
Dimasik2001 <dimonkalibaba@gmail.com>
2021-09-01 13:17:42 +0300

    Empty project

---
+++ Modul4HW3/Modul4HW3/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Modul4HW3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
