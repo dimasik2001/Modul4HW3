91b908c8fe0e4d888b3dc770187efb5b9feafa14
Dimasik2001 <dimonkalibaba@gmail.com>
2021-09-09 17:02:00 +0300

    Modul4HW5

--- Modul4HW3/Modul4HW3/Entities/Title.cs
+++ Modul4HW3/Modul4HW3/Entities/Title.cs
@@ -6,6 +6,6 @@ namespace Modul4HW3.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public List<Employee> Employees { get; set; } = new List<Employee>();
+        public virtual List<Employee> Employees { get; set; } = new List<Employee>();
     }
 }
